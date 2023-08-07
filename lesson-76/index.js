29664ae7b540a3b9146354092bd827b9148cf6db
Marti9969 <135505202+Marti9969@users.noreply.github.com>
2023-08-07 11:51:10 +0300

    Add files via upload

---
+++ lesson-76/index.js
@@ -0,0 +1,6 @@
+const firstName = 'Martin'
+const lastName = 'Shitc'
+let age = 24
+const City = 'Moscow'
+const Trained = 'AROKEN'
+console.log('Student ', firstName, lastName, 'y.o ', age, 'Resedent ', City, 'Baing-trained ', Trained)
